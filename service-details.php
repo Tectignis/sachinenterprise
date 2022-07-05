d06fef8165aad5ca120403f4e71949548ef33ce9
kajal281199 <97589538+kajal281199@users.noreply.github.com>
2022-07-05 14:07:14 +0530

    header & footer include

--- service-details.php
+++ service-details.php
@@ -22,121 +22,11 @@
 </head>
 <body>
 
-<div class="topbar">
-<div class="container">
-<div class="row">
-<div class="col-lg-9">
-<div class="topbar-left-side">
-<ul>
-<li><i class="fas fa-phone-alt"></i> <a href="tel:+13454567877">+1-3454-5678-77</a></li>
-<li><i class="fas fa-clock"></i> Mon To Fri 9:00am To 6:00pm</li>
-<li><i class="fas fa-map-marker-alt"></i> <a href="../../maps/search/2976 sunrise road las vegas/@36.1246738,-115.4551869,10z/data=!3m1!4b1-2.html">2976 Sunrise Avenue, Las Vegas, USA</a></li>
-</ul>
-</div>
-</div>
-<div class="col-lg-3">
-<div class="topbar-right-side">
-<div class="account">
-<a href="profile-authentication.html"><i class="fas fa-user"></i> Login</a>
-</div>
-<div class="language-select option-select-area">
-<i class="fas fa-globe"></i>
-<select>
-<option value="1">English</option>
-<option value="2">العربيّة</option>
-<option value="3">Dutch</option>
-<option value="4">Thai</option>
-<option value="5">简体中</option>
-</select>
-</div>
-</div>
-</div>
-</div>
-</div>
-</div>
 
 
-<div class="header-area">
-<div class="navbar-area">
 
-<div class="main-responsive-nav">
-<div class="container">
-<div class="mobile-nav">
-<a href="index.html" class="logo"><img src="assets/images/logo.png" alt="logo"></a>
-<ul class="menu-sidebar menu-small-device">
-<li><button class="popup-button"><i class="fas fa-search"></i></button></li>
-</ul>
-</div>
-</div>
-</div>
 
-<div class="main-nav">
-<div class="container">
-<nav class="navbar navbar-expand-md navbar-light">
-<a class="navbar-brand" href="index.html"> 
-<img src="assets/images/logo.png" alt="logo">
-</a>
-<div class="collapse navbar-collapse mean-menu" id="navbarSupportedContent">
-<ul class="navbar-nav">
-<li class="nav-item plus-icon">
-<a href="#" class="nav-link dropdown-toggle">Home <i class="fas fa-angle-down"></i></a>
-<ul class="dropdown-menu">
-<li class="nav-item"><a href="index.html" class="nav-link">Home Page 1</a></li>
-<li class="nav-item"><a href="index-2.html" class="nav-link">Home Page 2</a></li>
-<li class="nav-item"><a href="index-3.html" class="nav-link">Home Page 3</a></li>
-</ul>
-</li>
-<li class="nav-item">
-<a href="#" class="nav-link dropdown-toggle active">Services <i class="fas fa-angle-down"></i></a>
-<ul class="dropdown-menu">
-<li class="nav-item">
-<a href="#" class="nav-link dropdown-toggle">Services <i class="fas fa-angle-right"></i></a>
-<ul class="dropdown-menu">
-<li class="nav-item"><a href="services.html" class="nav-link">Services 01</a></li>
-<li class="nav-item"><a href="services-2.html" class="nav-link">Services 02</a></li>
-</ul>
-</li>
-<li class="nav-item"><a href="service-details.html" class="nav-link active">Service Details</a></li>
-</ul>
-</li>
-<li class="nav-item">
-<a href="#" class="nav-link dropdown-toggle">Pages <i class="fas fa-angle-down"></i></a>
-<ul class="dropdown-menu">
-<li class="nav-item"><a href="about.html" class="nav-link">About Us</a></li>
-<li class="nav-item"><a href="faq.html" class="nav-link">FAQ</a></li>
-<li class="nav-item"><a href="team.html" class="nav-link">Our Team</a></li>
-<li class="nav-item"><a href="gallery.html" class="nav-link">Gallery</a></li>
-<li class="nav-item"><a href="pricing.html" class="nav-link">Pricing Plan</a></li>
-<li class="nav-item"><a href="profile-authentication.html" class="nav-link">My Account</a></li>
-<li class="nav-item"><a href="error.html" class="nav-link">404 Error</a></li>
-<li class="nav-item"><a href="testimonial.html" class="nav-link">Testimonials</a></li>
-<li class="nav-item"><a href="privacy.html" class="nav-link">Privacy Policy</a></li>
-<li class="nav-item"><a href="terms.html" class="nav-link">Terms & Conditions</a></li>
-</ul>
-</li>
-<li class="nav-item">
-<a href="#" class="nav-link dropdown-toggle">Blog <i class="fas fa-angle-down"></i></a>
-<ul class="dropdown-menu">
-<li class="nav-item"><a href="blog.html" class="nav-link">Blog Grid</a></li>
-<li class="nav-item"><a href="blog-left-sidebar.html" class="nav-link">Blog Left Sidebar</a></li>
-<li class="nav-item"><a href="blog-right-sidebar.html" class="nav-link">Blog Right Sidebar</a></li>
-<li class="nav-item"><a href="blog-details.html" class="nav-link">Blog Details</a></li>
-</ul>
-</li>
-<li class="nav-item"><a href="contact.html" class="nav-link">Contact Us</a></li>
-</ul>
-<div class="menu-sidebar menu-sidebar-1">
-<ul>
-<li><button class="popup-button"><i class="fas fa-search"></i></button></li>
-<li><a class="default-button default-button-3" href="contact.html"><span>Get Started</span></a></li>
-</ul>
-</div>
-</div>
-</nav>
-</div>
-</div>
-</div>
-</div>
+<?php include("include/header.php"); ?>
 
 
 <div class="uni-banner">
@@ -340,11 +230,8 @@
 </div>
 
 
-<div class="copyright bg-000000">
-<div class="container">
-<p>Copyright &copy; 2021. <strong>ANWA</strong> All Rights Reserved By <a target="_blank" href="../../index-39.htm">HiBootstrap</a></p>
-</div>
-</div>
+
+<?php include("include/footer.php"); ?>
 
 
 <div class="popup">
