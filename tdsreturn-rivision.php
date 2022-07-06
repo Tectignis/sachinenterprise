8fce5326cfd69fcf92e7130eb9b1b05fa7a235d9
Madhuri Dere <madhuridere2018@gmail.com>
2022-07-06 18:08:04 +0530

    changes

---
+++ tdsreturn-rivision.php
@@ -0,0 +1,210 @@
+<?php
+include("include/config.php");
+
+?>
+
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="assets/css/animate.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/fontawsome.min.css">
+    <link rel="stylesheet" href="assets/fonts/font/flaticon.css">
+    <link rel="stylesheet" href="assets/css/meanmenu.min.css">
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/css/nice-select.css">
+    <link rel="stylesheet" href="assets/css/barfiller.css">
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+    <link rel="stylesheet" href="assets/css/odometer-theme-default.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/responsive.css">
+    <title>Anwa - Cleaning Company HTML Template</title>
+    <link rel="icon" type="image/png" href="assets/images/fav-icon.png">
+</head>
+
+<body>
+
+
+
+    <?php include("include/header.php"); ?>
+
+
+
+   
+
+
+
+
+    
+
+
+    
+
+
+    <!-- <div class="booking booking-1">
+<div class="container">
+<div class="row justify-content-center">
+<div class="col-lg-10">
+<div class="booking-content">
+<div class="default-section-title">
+<h3>Book A Service</h3>
+</div>
+<div class="section-content">
+<form>
+<div class="row">
+<div class="col-lg-6 col-md-6 col-sm-6 col-12">
+<div class="booking-form-content">
+<input type="text" class="form-control" placeholder="Your Name">
+</div>
+</div>
+<div class="col-lg-6 col-md-6 col-sm-6 col-12">
+<div class="booking-form-content">
+<input type="Email" class="form-control" placeholder="Email Address">
+</div>
+</div>
+<div class="col-lg-6 col-md-6 col-sm-6 col-12">
+<div class="booking-form-content">
+<input type="text" class="form-control" placeholder="Zip Code">
+</div>
+</div>
+<div class="col-lg-6 col-md-6 col-sm-6 col-12">
+<div class="booking-form-content">
+<input type="text" class="form-control" placeholder="Mobile Number">
+</div>
+</div>
+<div class="col-lg-6 col-md-6 col-sm-6 col-12">
+<div class="booking-form-content">
+<select class="form-select" aria-label="Default select example">
+<option selected="" disabled="">Choose A Service</option>
+<option value="1">Room Cleaning</option>
+<option value="2">Office Cleaning</option>
+<option value="3">Ground Cleaning</option>
+</select>
+</div>
+</div>
+<div class="col-lg-6 col-md-6 col-sm-6 col-12">
+<div class="booking-form-content">
+<button class="default-button" type="submit"><span>Submit Details</span></button>
+</div>
+</div>
+</div>
+</form>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div> -->
+
+
+    
+
+
+    <div class="pricing pricing-1 pt-100 shape-bg">
+        <div class="container">
+            <div class="default-section-title default-section-title-middle">
+                <h3>TDS Returns Revision</h3>
+                <br>
+                <h4>All you need to know</h4>
+            </div>
+            <div class="setion-content">
+                
+                    
+                        
+         
+                    <div class="row justify-content-center pt-3">
+                    <div class="col-8">               
+         <label>
+         TDS Return is required to be filed by any person who is liable to deduct tax at source.  A TDS Return is a quarterly statement which has to be submitted to the Income Tax Department of India. Submitting TDS Return is mandatory if you are a deductor. It should contain all details of TDS deducted and deposited by you for a particular quarter. 
+</label>
+</div>
+</div>
+
+            
+           
+<div class="row justify-content-center pt-3">
+                    <div class="col-8">
+           <label>You may need to file TDS correction or TDS revision if you have wrongly shown any details in original TDS Returns. Such mistakes can be wrong PAN, wrong date, or short or no tax deduction. In all such cases, in order to ensure seamless credit to the person whose TDS is deducted, you should revise your TDS Return.</label>
+</div>
+</div>
+          
+<div class="row justify-content-center pt-3">
+                    <div class="col-8">         
+           <label>You may also need to revise your TDS return if you have short paid any TDS and received any notice from income tax department. It should be noted that only by paying short deducted TDS, your work is not over. This has to be given effect through a correctly filed TDS statement.
+                </label>
+</div>
+</div>
+               
+<div class="row justify-content-center pt-3">
+                    <div class="col-8">
+            <lable>TDS return may be corrected as many times as needed. There is no due date by which a TDS return has to be revised. Hence, as soon as an error is discovered, you should file a correction return.</label>
+</div>
+</div>
+            
+          
+<div class="row justify-content-center pt-3">
+                    <div class="col-8">
+            <label>EbizFiling.com is an eminent business platform and a progressive concept, which helps end-to-end incorporation, compliance, advisory, and management consultancy services to clients in India and abroad. Filing TDS Return Revision is easy, seamless, error-free, cheapest and quickest with EbizFiling.com! You may get in touch with our compliance manager on 09643203209 or email info@ebizfiling.com for free consultation.</label>
+                    </div>
+                   
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+   
+
+
+    <?php include("include/newsletter.php");?>
+    <?php include("include/footer.php"); ?>
+
+
+    <div class="popup">
+        <div class="popup-content">
+            <button class="close-btn" id="popup-close"><i class="fas fa-times"></i></button>
+            <form>
+                <div class="input-group search-box">
+                    <input type="text" class="form-control" placeholder="Search">
+                    <button class="btn" type="submit"><i class="fas fa-search"></i></button>
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <div class="go-top"><img src="assets/images/png/002-top.png" alt=""></div>
+
+
+    <script data-cfasync="false" src="../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min-7.js"></script>
+    <script src="assets/js/jquery-3.6.0.min.js"></script>
+
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+
+    <script src="assets/js/meanmenu.js"></script>
+
+    <script src="assets/js/owl.carousel.min.js"></script>
+
+    <script src="assets/js/jquery.magnific-popup.min.js"></script>
+
+    <script src="assets/js/jquery.nice-select.min.js"></script>
+
+    <script src="assets/js/form-validator.min.js"></script>
+    <script src="assets/js/contact-form-script.js"></script>
+
+    <script src="assets/js/appear.min.js"></script>
+
+    <script src="assets/js/TweenMax.js"></script>
+
+    <script src="assets/js/odometer.min.js"></script>
+
+    <script src="assets/js/jquery.barfiller.js"></script>
+
+    <script src="assets/js/custom.js"></script>
+</body>
+
+</html>
