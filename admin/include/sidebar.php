28d2ea5f2016e478cc08c3d93b0346b3633d01e5
Govind Bavkar <tectignisofficial@gmail.com>
2022-07-07 14:06:12 +0530

    update

--- admin/include/sidebar.php
+++ admin/include/sidebar.php
@@ -25,7 +25,7 @@
             <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
             <li class="nav-item">
-              <a href="#" class="nav-link">
+              <a href="index.php" class="nav-link">
                 <i class="nav-icon fas fa-tachometer-alt text-red"></i>
                 <p>
                   Dashboard
