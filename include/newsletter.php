072cd2cb87449508ca509f35850c48c52c2dd4f8
Madhuri Dere <madhuridere2018@gmail.com>
2022-07-09 11:43:00 +0530

    chnge

--- include/newsletter.php
+++ include/newsletter.php
@@ -1,3 +1,17 @@
+<?php
+include("config.php");
+
+if(isset($_POST['sub'])){
+
+    $name=$_POST['name'];
+    $phonenumber=$_POST['phonenumber'];
+
+    $sql=mysqli_query($conn,"INSERT INTO `callback`(`name`,`phonenumber`)
+     VALUES('$name','$phonenumber')");
+}
+
+?>
+
 <div class="newsletter newsletter-1">
     <div class="container">
         <div class="newsletter-content-area">
@@ -9,21 +23,21 @@
                     </div>
                 </div>
                 <div class="col-lg-8 col-md-12 col-sm-12 col-12">
-                    <form>
+                    <form method="POST">
                         <div class="row align-items-center justify-content-center">
                             <div class="col-lg-4 col-md-6 col-sm-6 col-12">
                                 <div class="news-input-card">
-                                    <input type="text" class="form-control" placeholder="Your Name">
+                                    <input type="text" class="form-control" placeholder="Your Name" name="name">
                                 </div>
                             </div>
                             <div class="col-lg-4 col-md-6 col-sm-6 col-12">
                                 <div class="news-input-card">
-                                    <input type="text" class="form-control" placeholder="Your Mobile No.">
+                                    <input type="text" class="form-control" placeholder="Your Mobile No." name="phonenumber">
                                 </div>
                             </div>
                             <div class="col-lg-4 col-md-6 col-sm-6 col-12">
                                 <div class="news-input-card">
-                                    <button class="default-button"><span>Submit</span></button>
+                                    <button class="default-button" name="sub"><span>Submit</span></button>
                                 </div>
                             </div>
                         </div>
