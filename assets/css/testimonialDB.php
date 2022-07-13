0439a688b4b0d4ebaa577143e16944f0e60aeb75
kajal281199 <97589538+kajal281199@users.noreply.github.com>
2022-07-13 17:00:14 +0530

    change

--- assets/css/testimonialDB.php
+++ assets/css/testimonialDB.php
@@ -1,25 +1,25 @@
 <?php
-    include('../../Sachin enterprise/assets/include/config.php');
+  include("../include/config.php");
     if(isset($_POST['save']))
 {
     $name=$_POST['name'];
     $position=$_POST['position'];
-    $upload_icon=$_FILES['image']['name'];
-    $extension=substr( $upload_icon,strlen( $upload_icon)-4,strlen( $upload_icon));
+    $image=$_FILES['image']['name'];
+    $extension=substr( $image,strlen( $image)-4,strlen( $image));
     $all_extension = array(".jpg","jpeg",".png","gif");
     if(!in_array($extension,$all_extension)){
       $msg="Invalid format. Only jpg / jpeg/ png /gif format allowed";
     } 
         else{
-    $upload_icon=md5($upload_icon).$extension;
+    $image=md5($image).$extension;
     $dnk=$_FILES['image']['tmp_name'];
-    $loc="image/".$upload_icon;
+    $loc="image/".$image;
     move_uploaded_file($dnk,$loc);
         }
     $rating=$_POST['rating'];
     $message=$_POST['message'];
 
-    $sql="INSERT INTO `testimonial`(`name`, `position`, `image`, `rating`, `message`) VALUES('$name', '$position', '$upload_icon', '$rating', '$message')";
+    $sql="INSERT INTO `testimonial`(`name`, `position`, `image`, `rating`, `message`) VALUES('$name', '$position', '$image', '$rating', '$message')";
 
     if(mysqli_query($conn,$sql)){
         echo "<script> alert('New Records Added Has Been Succesfully !');window.location='testimonial1.php';</script>";
