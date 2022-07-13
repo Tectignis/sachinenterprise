65e67e352a177baab8048e7fb85510402e4e3d8f
aniketnangare23 <aniketnangare23@gmail.com>
2022-07-13 16:19:28 +0530

    changes

--- services.php
+++ services.php
@@ -58,9 +58,9 @@ include("include/config.php");
                          while($arr=mysqli_fetch_array($sql)){
                         ?>
                     <div class="col-lg-4 col-md-6 col-sm-12 col-12">
-                        <div class="service-card">
+                        <div class="service-card pt-5">
                             <div class="service-img">
-                                <a href="service-details.html"><?php echo $arr['image'];?></a>
+                                <a href="service-details.html"></a>
                             </div>
                             <div class="service-card-text">
                                 <div class="service-card-icon">
