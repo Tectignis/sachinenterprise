8132e18753bec289f30df0d4b99b70a9f4c5a8fb
aniketnangare23 <aniketnangare23@gmail.com>
2022-07-14 15:03:57 +0530

    returns res.

--- esi-return.php
+++ esi-return.php
@@ -59,7 +59,7 @@ echo 'connection failed';
       <div class="card" style="border:none">
         <div class="card-body">
         <div class="row">
-          <div class="col-4 pt-5">
+          <div class="col-sm-8 col-md-8 col-lg-4 pt-5">
                 <h2 class="title ">File Your</h2>
                 <h5 class="subtitle  ">ESI Returns</h5>
                 <p></p>
@@ -67,10 +67,10 @@ echo 'connection failed';
                   <label for="">File error-free ESI Returns and in-time. File your ESI returns through experts sachinenterpraise.in Prices start at<strong>INR 999/- only.</strong></label>
                 </div>
                 </div>
-                <div class="col-4">
+                <div class="col-sm-4 col-md-4 col-lg-4">
                 <img src="../../sachinenterprise\assets\images\return-img\rivision.jpg" alt="sachinenterprise">
             </div>
-            <div class="col-4">
+            <div class="col-sm-12 col-md-12 col-lg-4">
             <div class="card ">
             <div class=" shadow bg-white rounded">
               <div class="card-header text-center" style="background-color:red; color:white">
@@ -181,7 +181,7 @@ echo 'connection failed';
             <h2 style="text-align:center">Simple Prices | No Surprises</h2>
     <div class="card-body">
         <div class="row ">
-            <div class="col-4">
+            <div class="col-sm-12 col-md-12 col-lg-4 py-2">
             <div class="card h-100 " >
               <div class="card card-header text-center" style="background-color:#000080; color:white">
                 <h4 class="card-title">ESSENTIAL</h4>
@@ -201,7 +201,7 @@ echo 'connection failed';
                 </div>
             </div>
             </div>
-            <div class="col-4">
+            <div class="col-sm-12 col-md-12 col-lg-4 py-2">
             <div class=" card h-100" >
               <div class="card card-header text-center" style="background-color:#000080; color:white">
                 <h4 class="card-title">ENHANCED</h4>
@@ -221,7 +221,7 @@ echo 'connection failed';
                 </div>
             </div>
             </div>
-            <div class="col-4">
+            <div class="col-sm-12 col-md-12 col-lg-4  py-2">
             <div class=" card h-100" >
               <div class=" card card-header text-center" style="background-color:#000080; color:white">
                 <h4 class="card-title">ULTIMATE</h4>
