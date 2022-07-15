465d4a0db633663eeb17636d1f3ea32f12cb8133
Madhuri Dere <madhuridere2018@gmail.com>
2022-07-15 17:55:48 +0530

    cf

--- terms.php
+++ terms.php
@@ -1,4 +1,10 @@
-﻿<!DOCTYPE html>
+﻿<?php
+include("include/config.php"); 
+$sql=mysqli_query($conn,"select * from `general_setting`");
+$arr=mysqli_fetch_array($sql)
+?>
+
+<!DOCTYPE html>
 <html lang="zxx">
 
 <head>
@@ -18,8 +24,8 @@
     <link rel="stylesheet" href="assets/css/odometer-theme-default.css">
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="assets/css/responsive.css">
-    <title>Anwa - Cleaning Company HTML Template</title>
-    <link rel="icon" type="image/png" href="assets/images/fav-icon.png">
+    <title>Sachin Enterprises -terms </title>
+<link rel="icon" type="image/png" href="admin/logo/<?php echo $arr['select_favicon'];?>">
 </head>
 
 <body>
